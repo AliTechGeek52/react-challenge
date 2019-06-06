8d186de8c73f95566d79f4992440e4cd0d8228dc
AliRaza <32015381+AliTechGeek52@users.noreply.github.com>
2019-06-07 02:55:02 +0500

    Updates

---
+++ precache-manifest.883fa3287419c925d2d7557d867d03c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74cb927df8d205dd0b27c0283b894f4b",
+    "url": "/react-challenge/index.html"
+  },
+  {
+    "revision": "3e46b9f0e3d8a6fdabf4",
+    "url": "/react-challenge/static/css/main.83902452.chunk.css"
+  },
+  {
+    "revision": "0b8c96f9de777eef4f60",
+    "url": "/react-challenge/static/js/2.5ecca776.chunk.js"
+  },
+  {
+    "revision": "3e46b9f0e3d8a6fdabf4",
+    "url": "/react-challenge/static/js/main.0a652031.chunk.js"
+  },
+  {
+    "revision": "87862c6bdc1516f59c4b",
+    "url": "/react-challenge/static/js/runtime~main.d3047029.js"
+  },
+  {
+    "revision": "2899e6a661969af788bf69073edbe20c",
+    "url": "/react-challenge/static/media/product-1.2899e6a6.jpg"
+  },
+  {
+    "revision": "2f9f5891f91a0992568bef1169f83f2e",
+    "url": "/react-challenge/static/media/product-2.2f9f5891.jpg"
+  }
+]);
